f00d40e272ec543280cc1c0bdfea274ca6e73e5f
williamfaraday123 <williamfaraday123@gmail.com>
2025-03-17 19:46:13 +0800

    update

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            I am a Computer Science undergraduate in NTU with a strong interest in computing. My academic projects involve developing apps with React, Node.js, Next.js, SQL, and AWS, conducting data analysis with Python and Jupyter Notebook, as well as designing database systems with SQL and no SQL. During my projects I have developed a passion in developing apps to improve the user experience, creating robust data storage systems, and training AI models with complex algorithms. I would love to discuss how my skills can support your company.
+            I am a Computer Science undergraduate in Nanyang Technological University with a strong interest in computing. My projects involve training and fine-tuning AI models with Pytorch and Tensorflow, developing apps with React, Node.js, Next.js, SQL, and AWS, conducting data analysis with Python and Jupyter Notebook / Google Colab, as well as designing database systems with SQL. During my projects I have developed a passion in developing apps to improve the user experience, creating robust data storage systems, and training AI models with complex algorithms. I would love to discuss how my skills can support your company.
         </div>
     );
 };
--- src/screens/leftScreen/LeftScreen.jsx
+++ src/screens/leftScreen/LeftScreen.jsx
@@ -5,7 +5,7 @@ const LeftScreen = ({ handleVisibleComponent }) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.name}>Hi, my name is Isaac</h1>
-            <h3 className={styles.role}>University student, Software developer</h3>
+            <h3 className={styles.role}>AI Software developer, <br/> Computer Science Undergraduate</h3>
             <p className={styles.description}>I build accessible digital experiences for the web.</p>
             {["About", "Experience", "Projects"].map((component, index) => (
                 <div key={index} className={styles.menuItem}>
@@ -14,6 +14,7 @@ const LeftScreen = ({ handleVisibleComponent }) => {
             ))}
             <a href='https://github.com/williamfaraday123' className={styles.link}>Github</a>
             <a href="mailto:isaa0015@e.ntu.edu.sg" className={styles.link}>Email</a>
+            <a href='https://www.linkedin.com/in/isaaclim2/' className={styles.link}>LinkedIn</a>
         </div>
     )
 };
