9083ce5f31305a02f0a03c52a8cf6ed62fda1934
williamfaraday123 <williamfaraday123@gmail.com>
2025-03-17 19:26:39 +0800

    update

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            I am a proactive Computer Science undergraduate in NTU with a strong interest in computing. My academic projects involve developing apps with React, Node.js, Next.js, SQL, and AWS, conducting data analysis with Python and Jupyter Notebook, as well as designing database systems with SQL and no SQL. During my projects I have developed a passion in developing apps to improve the user experience, creating robust data storage systems, and training AI models with complex algorithms. I would love to discuss how my skills can support your company.
+            I am a Computer Science undergraduate in NTU with a strong interest in computing. My academic projects involve developing apps with React, Node.js, Next.js, SQL, and AWS, conducting data analysis with Python and Jupyter Notebook, as well as designing database systems with SQL and no SQL. During my projects I have developed a passion in developing apps to improve the user experience, creating robust data storage systems, and training AI models with complex algorithms. I would love to discuss how my skills can support your company.
         </div>
     );
 };
